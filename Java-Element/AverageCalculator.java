152e033356a7d3bb4ecc2489d9095f769adfe5a5
Jag mohan sihora <157011452+jagmohansihora12@users.noreply.github.com>
2025-07-30 12:51:52 +0530

    resolve conflict

--- Java-Element/AverageCalculator.java
+++
@@ -1,9 +0,0 @@
-public class AverageCalculator {
-    public static void main(String[] args) {
-        int physics = 95;
-        int chemistry = 96;
-        int maths = 94;
-        double average = (physics + chemistry + maths) / 3.0;
-        System.out.println(average);
-    }
-}
