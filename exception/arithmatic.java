78c848b33c0e0fcc895e0965107adf78a7ed7d15
Jag mohan sihora <157011452+jagmohansihora12@users.noreply.github.com>
2025-08-07 09:27:04 +0530

    exception

---
+++ exception/arithmatic.java
@@ -0,0 +1,12 @@
+public class arithmatic {
+    public static void main(String[] args) {
+        try{
+
+            int a = 10;
+            int b = a / 0;
+        }
+        catch(ArithmeticException e){
+            System.out.println("Arithmetic Exception: Division by zero is not allowed.");
+        }
+    }
+}
